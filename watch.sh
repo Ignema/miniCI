b3c0d3806762fa8f0365f72533d69450ded1b45a
Yasser Douslimi <yassir_douslimi@um5.ac.ma>
2022-10-21 15:18:12 +0000

    ⭕ Added pip verification in watch script

--- watch.sh
+++ watch.sh
@@ -1,4 +1,5 @@
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 pip install pipenv
 pipenv install
-pipenv shell
-nohup uvicorn main:app --host 0.0.0.0 --port 3934 &
+nohup pipenv run uvicorn main:app --port 3934 &
