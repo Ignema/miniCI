6aad1aede57441dcc3e280ec192636d0e6724e66
Yasser Douslimi <yassir_douslimi@um5.ac.ma>
2022-10-21 15:29:17 +0000

    🥲 fixed import & list bug in cmd.py

--- lib/cmd.py
+++ lib/cmd.py
@@ -1,4 +1,6 @@
+import shlex
+import subprocess
 from typing import List
 
 def exec_command(cmd: List[str]) -> None:
-    subprocess.run(shlex.split(cmd))
+    subprocess.run(shlex.split(" ".join(cmd)))
